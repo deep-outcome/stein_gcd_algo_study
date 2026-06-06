11faf43f58da1f2695578ba64c0049047996302b
Mr. Deepest Outcome <uzivatel919@seznam.cz>
2026-06-07 01:10:03 +0200

    corrections

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Stein's greatest common divisor algorithm study
+# Stein's Greatest Common Divisor Algorithm Study
 Simple dive into Stein's greatest common divisor (GCD) algorithm, also known as Binary GCD algorithm, created by Josef Stein in 1967.
 
 - See [Wikipedia export](./stein-gcd-analysis.pdf) for analysis.
@@ -22,7 +22,7 @@ Orientative benchmark sample, no extended statistical measurement applied.
 Configuration:
 <ul>
 <li>Operating System: openSUSE Leap 16.0</li>
-<li>Kernel Version: 6.12.0-160000.28-default (64-bit)</li>
+<li>Kernel Version:  6.12.0-160000.33-default (64-bit)</li>
 <li>Processors: 16 × AMD Ryzen 7 3800X 8-Core Processor</li>
 </ul>
 </small>
@@ -34,15 +34,11 @@ some conditions these tests can reach times similar to Euclidean GCD algorithms 
 
 ```rust
 #[bench]
-fn extended_test(b: &mut Bencher) {
+fn method(b: &mut Bencher) {
     let num_1 = 2_559_031_471; // 150531263ᵖ ⋅17ᵖ
-    let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
+    let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ    
 
-    b.iter(|| {
-        let gcd = method(num_1, num_2);
-        assert_eq!(proof, gcd);
-    });
+    b.iter(|| _ = method(num_1, num_2));
 }
 ```
 
@@ -56,14 +52,14 @@ and other factors.
 
 |         Method          |                Description            |   Mean   | Deviation |
 |-------------------------|---------------------------------------|----------|-----------|
-| gcd_disingenuous_2      | Optimized iterative Stein's GCD       | 19.24 ns | ± 0.10    |
-| gcd_disingenuous        | Iterative Stein's GCD                 | 35.67 ns | ± 0.24    |
-| gcd_e                   | Euclidean GCD                         | 54.95 ns | ± 0.49    |
-| gcd_ee                  | Extended Euclidean GCD                | 55.15 ns | ± 0.93    |
-| gcd_naive_2             | Recursive (still naive) but optimized | 47.37 ns | ± 4.61    |
-| gcd_naive               | Very naive recursive implementation   | 66.11 ns | ± 0.83    |
-| gcd_nonnaive_extended_2 | Extended optimized Stein's GCD        | 72.58 ns | ± 5.40    |
-| gcd_nonnaive_extended   | Extended optimized Stein's GCD        | 75.18 ns | ± 6.26    |
+| gcd_disingenuous_2      | Optimized iterative Stein's GCD       | 18.91 ns | ± 0.23    |
+| gcd_disingenuous        | Iterative Stein's GCD                 | 35.34 ns | ± 0.26    |
+| gcd_e                   | Euclidean GCD                         | 54.94 ns | ± 0.36    |
+| gcd_ee                  | Extended Euclidean GCD                | 54.97 ns | ± 0.34    |
+| gcd_naive_2             | Recursive (still naive) but optimized | 42.85 ns | ± 7.16    |
+| gcd_naive               | Very naive recursive implementation   | 60.69 ns | ± 1.18    |
+| gcd_nonnaive_extended_2 | Extended optimized Stein's GCD        | 65.39 ns | ± 10.21   |
+| gcd_nonnaive_extended   | Extended optimized Stein's GCD        | 69.05 ns | ± 12.73   |
 
 ## Inquisitive Spirit Never Gives Up
 Because inquisitive spirit never gives up, one more iteration of optimizations to `gcd_nonnaive_extended` earns intended stabilization of branch prediction.
--- tests/bench.rs
+++ tests/bench.rs
@@ -11,97 +11,65 @@ use test::Bencher;
 #[bench]
 fn gcd_naive_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471u64; // 150531263ᵖ ⋅17ᵖ
-    let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
+    let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ    
 
-    b.iter(|| {
-        let gcd = gcd_naive(num_1, num_2);
-        assert_eq!(proof, gcd);
-    });
+    b.iter(|| _ = gcd_naive(num_1, num_2));
 }
 
 #[bench]
 fn gcd_naive_2_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471u64; // 150531263ᵖ ⋅17ᵖ
     let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
 
-    b.iter(|| {
-        let gcd = gcd_naive_2(num_1, num_2);
-        assert_eq!(proof, gcd);
-    });
+    b.iter(|| _ = gcd_naive_2(num_1, num_2));
 }
 
 #[bench]
 fn gcd_disingenuous_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471u64; // 150531263ᵖ ⋅17ᵖ
     let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
 
-    b.iter(|| {
-        let gcd = gcd_disingenuous(num_1, num_2);
-        assert_eq!(proof, gcd);
-    });
+    b.iter(|| _ = gcd_disingenuous(num_1, num_2));
 }
 
 #[bench]
 fn gcd_disingenuous_2_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471u64; // 150531263ᵖ ⋅17ᵖ
     let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
 
-    b.iter(|| {
-        let gcd = gcd_disingenuous_2(num_1, num_2);
-        assert_eq!(proof, gcd);
-    });
+    b.iter(|| _ = gcd_disingenuous_2(num_1, num_2));
 }
 
 #[bench]
 fn gcd_e_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471u64; // 150531263ᵖ ⋅17ᵖ
     let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
 
-    b.iter(|| {
-        let gcd = gcd_e(num_1, num_2);
-        assert_eq!(proof, gcd);
-    });
+    b.iter(|| _ = gcd_e(num_1, num_2));
 }
 
 #[bench]
 fn gcd_nonnaive_extended_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471i64; // 150531263ᵖ ⋅17ᵖ
     let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
 
-    b.iter(|| {
-        let gcd = gcd_nonnaive_extended(num_1, num_2);
-        assert_eq!(proof, gcd.0);
-    });
+    b.iter(|| _ = gcd_nonnaive_extended(num_1, num_2));
 }
 
 #[bench]
 fn gcd_nonnaive_extended_2_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471i64; // 150531263ᵖ ⋅17ᵖ
     let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
 
-    b.iter(|| {
-        let gcd = gcd_nonnaive_extended_2(num_1, num_2);
-        assert_eq!(proof, gcd.0);
-    });
+    b.iter(|| _ = gcd_nonnaive_extended_2(num_1, num_2));
 }
 
 #[bench]
 fn gcd_ee_test(b: &mut Bencher) {
     let num_1 = 2_559_031_471i64; // 150531263ᵖ ⋅17ᵖ
     let num_2 = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-    let proof = 1;
 
-    b.iter(|| {
-        let gcd = gcd_ee(num_1, num_2);
-        assert_eq!(proof, gcd.0);
-    });
+    b.iter(|| _ = gcd_ee(num_1, num_2));
 }
 
 fn gcd_e(mut gcd: u64, mut rem: u64) -> u64 {
@@ -148,292 +116,295 @@ fn gcd_ee(mut gcd: i64, mut rem: i64) -> (i64, i128, i128) {
     (gcd, x as i128, y as i128)
 }
 
-mod gcd_e {
-    use super::gcd_e;
+mod tests_of_units {
 
-    #[test]
-    fn coprime_primes_test() {
-        // both prime numbers
-        let l_hand = 1_299_709;
-        let r_hand = 56_999;
+    mod gcd_e {
+        use super::super::gcd_e;
 
-        let proof = 1;
+        #[test]
+        fn coprime_primes_test() {
+            // both prime numbers
+            let l_hand = 1_299_709;
+            let r_hand = 56_999;
 
-        let res = gcd_e(l_hand, r_hand);
-        assert_eq!(proof, res);
+            let proof = 1;
+
+            let res = gcd_e(l_hand, r_hand);
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn coprime_odd_test() {
+            let l_hand = 2_559_031_471u64; // 150531263ᵖ ⋅17ᵖ
+            let r_hand = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
+
+            let proof = 1;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn coprime_mixed_a_test() {
+            let l_hand = 52_685_751_650_u64; // 150530719ᵖ ⋅350ᶜ
+            let r_hand = 52_535_230_703_u64; // 150530747ᵖ ⋅349ᵖ
+
+            let proof = 1;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn coprime_mixed_b_test() {
+            let l_hand = 19_209_934_347_u64; // 56666473ᵖ ⋅113ᵖ ⋅3ᵖ
+            let r_hand = 10_993_312_058_u64; // 56666557ᵖ ⋅2ᵖ ⋅97ᵖ
+
+            let proof = 1;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn not_coprime_mixed_a_test() {
+            let l_hand = 37_683_426; // 570961ᵖ ⋅66ᶜ
+            let r_hand = 18_804_423; // 569831ᵖ ⋅33ᶜ
+
+            let proof = 33;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn not_coprime_mixed_b_test() {
+            let l_hand = 1_822_623; // 5021ᵖ ⋅33ᶜ ⋅11ᵖ
+            let r_hand = 1_650_990; // 5003ᵖ ⋅10ᶜ ⋅33ᶜ
+
+            let proof = 33;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn not_coprime_extra_test() {
+            let l_hand = 55_286_231; // 5021ᵖ ⋅77ᶜ ⋅11ᵖ ⋅13ᵖ
+            let r_hand = 7_704_620; // 5003ᵖ ⋅10ᶜ ⋅154ᶜ
+
+            let proof = 77;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn not_coprime_divisor_is_gcd_a() {
+            let l_hand = 777_777_777;
+            let r_hand = 111_111_111;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(r_hand, res);
+        }
+
+        #[test]
+        fn not_coprime_divisor_is_gcd_b() {
+            let num = 777_777_777;
+
+            let res = gcd_e(num, num);
+            assert_eq!(num, res);
+        }
+
+        #[test]
+        fn not_coprime_odd_a_test() {
+            let l_hand = 3_150_055_839u64; // 150002659ᵖ ⋅7ᵖ ⋅3ᵖ
+            let r_hand = 76_604_397; // 1502047ᵖ ⋅17ᵖ ⋅3ᵖ
+
+            let proof = 3;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn not_coprime_odd_b_test() {
+            let l_hand = 56_991; // 157ᵖ ⋅33ᶜ ⋅11ᵖ
+            let r_hand = 49_599; // 167ᵖ ⋅9ᶜ ⋅33ᶜ
+
+            let proof = 33;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn not_coprime_even_a_test() {
+            let l_hand = 549_755_813_888u64; // 2³⁹
+            let r_hand = 300_005_318; // 150002659ᵖ ⋅2ᵖ
+
+            let proof = 2;
+
+            let res = gcd_e(l_hand, r_hand);
+
+            assert_eq!(proof, res);
+        }
+
+        #[test]
+        fn not_coprime_even_b_test() {
+            let l_hand = 549_755_813_888u64; // 2³⁹
+            let r_hand = 33_554_432; // 2²⁵            
+
+            let res = gcd_e(l_hand, r_hand);
+            assert_eq!(r_hand, res);
+        }
     }
 
-    #[test]
-    fn coprime_odd_test() {
-        let l_hand = 2_559_031_471u64; // 150531263ᵖ ⋅17ᵖ
-        let r_hand = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
+    mod gcd_ee {
+        use super::super::gcd_ee;
 
-        let proof = 1;
+        fn run_test(l_hand: i64, r_hand: i64, proof: i64) {
+            let res = gcd_ee(l_hand, r_hand);
+            assert_eq!(proof, res.0);
 
-        let res = gcd_e(l_hand, r_hand);
+            let (x, y) = (res.1, res.2);
 
-        assert_eq!(proof, res);
-    }
+            let test = x * l_hand as i128 + y * r_hand as i128;
+            assert_eq!(proof as i128, test);
+        }
 
-    #[test]
-    fn coprime_mixed_a_test() {
-        let l_hand = 52_685_751_650_u64; // 150530719ᵖ ⋅350ᶜ
-        let r_hand = 52_535_230_703_u64; // 150530747ᵖ ⋅349ᵖ
+        #[test]
+        fn coprime_primes_test() {
+            // both prime numbers
+            let l_hand = 1_299_709;
+            let r_hand = 56_999;
 
-        let proof = 1;
+            let proof = 1;
 
-        let res = gcd_e(l_hand, r_hand);
+            run_test(l_hand, r_hand, proof)
+        }
 
-        assert_eq!(proof, res);
-    }
+        #[test]
+        fn coprime_odd_test() {
+            let l_hand = 2_559_031_471i64; // 150531263ᵖ ⋅17ᵖ
+            let r_hand = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
 
-    #[test]
-    fn coprime_mixed_b_test() {
-        let l_hand = 19_209_934_347_u64; // 56666473ᵖ ⋅113ᵖ ⋅3ᵖ
-        let r_hand = 10_993_312_058_u64; // 56666557ᵖ ⋅2ᵖ ⋅97ᵖ
+            let proof = 1;
 
-        let proof = 1;
+            run_test(l_hand, r_hand, proof)
+        }
 
-        let res = gcd_e(l_hand, r_hand);
+        #[test]
+        fn coprime_mixed_a_test() {
+            let l_hand = 52_685_751_650_i64; // 150530719ᵖ ⋅350ᶜ
+            let r_hand = 52_535_230_703_i64; // 150530747ᵖ ⋅349ᵖ
 
-        assert_eq!(proof, res);
-    }
+            let proof = 1;
 
-    #[test]
-    fn not_coprime_mixed_a_test() {
-        let l_hand = 37_683_426; // 570961ᵖ ⋅66ᶜ
-        let r_hand = 18_804_423; // 569831ᵖ ⋅33ᶜ
+            run_test(l_hand, r_hand, proof)
+        }
 
-        let proof = 33;
+        #[test]
+        fn coprime_mixed_b_test() {
+            let l_hand = 19_209_934_347_i64; // 56666473ᵖ ⋅113ᵖ ⋅3ᵖ
+            let r_hand = 10_993_312_058_i64; // 56666557ᵖ ⋅2ᵖ ⋅97ᵖ
 
-        let res = gcd_e(l_hand, r_hand);
+            let proof = 1;
 
-        assert_eq!(proof, res);
-    }
+            run_test(l_hand, r_hand, proof)
+        }
 
-    #[test]
-    fn not_coprime_mixed_b_test() {
-        let l_hand = 1_822_623; // 5021ᵖ ⋅33ᶜ ⋅11ᵖ
-        let r_hand = 1_650_990; // 5003ᵖ ⋅10ᶜ ⋅33ᶜ
+        #[test]
+        fn not_coprime_mixed_a_test() {
+            let l_hand = 37_683_426; // 570961ᵖ ⋅66ᶜ
+            let r_hand = 18_804_423; // 569831ᵖ ⋅33ᶜ
 
-        let proof = 33;
+            let proof = 33;
 
-        let res = gcd_e(l_hand, r_hand);
+            run_test(l_hand, r_hand, proof)
+        }
 
-        assert_eq!(proof, res);
-    }
+        #[test]
+        fn not_coprime_mixed_b_test() {
+            let l_hand = 1_822_623; // 5021ᵖ ⋅33ᶜ ⋅11ᵖ
+            let r_hand = 1_650_990; // 5003ᵖ ⋅10ᶜ ⋅33ᶜ
 
-    #[test]
-    fn not_coprime_extra_test() {
-        let l_hand = 55_286_231; // 5021ᵖ ⋅77ᶜ ⋅11ᵖ ⋅13ᵖ
-        let r_hand = 7_704_620; // 5003ᵖ ⋅10ᶜ ⋅154ᶜ
+            let proof = 33;
 
-        let proof = 77;
+            run_test(l_hand, r_hand, proof)
+        }
 
-        let res = gcd_e(l_hand, r_hand);
+        #[test]
+        fn not_coprime_extra_test() {
+            let l_hand = 55_286_231; // 5021ᵖ ⋅77ᶜ ⋅11ᵖ ⋅13ᵖ
+            let r_hand = 7_704_620; // 5003ᵖ ⋅10ᶜ ⋅154ᶜ
 
-        assert_eq!(proof, res);
-    }
+            let proof = 77;
 
-    #[test]
-    fn not_coprime_divisor_is_gcd_a() {
-        let l_hand = 777_777_777;
-        let r_hand = 111_111_111;
+            run_test(l_hand, r_hand, proof)
+        }
 
-        let res = gcd_e(l_hand, r_hand);
+        #[test]
+        fn not_coprime_divisor_is_gcd_a() {
+            let l_hand = 777_777_777;
+            let r_hand = 111_111_111;
 
-        assert_eq!(r_hand, res);
-    }
+            run_test(l_hand, r_hand, r_hand)
+        }
 
-    #[test]
-    fn not_coprime_divisor_is_gcd_b() {
-        let num = 777_777_777;
+        #[test]
+        fn not_coprime_divisor_is_gcd_b() {
+            let num = 777_777_777;
 
-        let res = gcd_e(num, num);
-        assert_eq!(num, res);
-    }
+            run_test(num, num, num)
+        }
 
-    #[test]
-    fn not_coprime_odd_a_test() {
-        let l_hand = 3_150_055_839u64; // 150002659ᵖ ⋅7ᵖ ⋅3ᵖ
-        let r_hand = 76_604_397; // 1502047ᵖ ⋅17ᵖ ⋅3ᵖ
+        #[test]
+        fn not_coprime_odd_a_test() {
+            let l_hand = 3_150_055_839i64; // 150002659ᵖ ⋅7ᵖ ⋅3ᵖ
+            let r_hand = 76_604_397; // 1502047ᵖ ⋅17ᵖ ⋅3ᵖ
 
-        let proof = 3;
+            let proof = 3;
 
-        let res = gcd_e(l_hand, r_hand);
+            run_test(l_hand, r_hand, proof)
+        }
 
-        assert_eq!(proof, res);
-    }
+        #[test]
+        fn not_coprime_odd_b_test() {
+            let l_hand = 56_991; // 157ᵖ ⋅33ᶜ ⋅11ᵖ
+            let r_hand = 49_599; // 167ᵖ ⋅9ᶜ ⋅33ᶜ
 
-    #[test]
-    fn not_coprime_odd_b_test() {
-        let l_hand = 56_991; // 157ᵖ ⋅33ᶜ ⋅11ᵖ
-        let r_hand = 49_599; // 167ᵖ ⋅9ᶜ ⋅33ᶜ
+            let proof = 33;
 
-        let proof = 33;
+            run_test(l_hand, r_hand, proof)
+        }
 
-        let res = gcd_e(l_hand, r_hand);
+        #[test]
+        fn not_coprime_even_a_test() {
+            let l_hand = 549_755_813_888i64; // 2³⁹
+            let r_hand = 300_005_318; // 150002659ᵖ ⋅2ᵖ
 
-        assert_eq!(proof, res);
-    }
+            let proof = 2;
 
-    #[test]
-    fn not_coprime_even_a_test() {
-        let l_hand = 549_755_813_888u64; // 2³⁹
-        let r_hand = 300_005_318; // 150002659ᵖ ⋅2ᵖ
+            run_test(l_hand, r_hand, proof)
+        }
 
-        let proof = 2;
+        #[test]
+        fn not_coprime_even_b_test() {
+            let l_hand = 549_755_813_888i64; // 2³⁹
+            let r_hand = 33_554_432; // 2²⁵            
 
-        let res = gcd_e(l_hand, r_hand);
-
-        assert_eq!(proof, res);
-    }
-
-    #[test]
-    fn not_coprime_even_b_test() {
-        let l_hand = 549_755_813_888u64; // 2³⁹
-        let r_hand = 33_554_432; // 2²⁵            
-
-        let res = gcd_e(l_hand, r_hand);
-        assert_eq!(r_hand, res);
-    }
-}
-
-mod gcd_ee {
-    use super::gcd_ee;
-
-    fn run_test(l_hand: i64, r_hand: i64, proof: i64) {
-        let res = gcd_ee(l_hand, r_hand);
-        assert_eq!(proof, res.0);
-
-        let (x, y) = (res.1, res.2);
-
-        let test = x * l_hand as i128 + y * r_hand as i128;
-        assert_eq!(proof as i128, test);
-    }
-
-    #[test]
-    fn coprime_primes_test() {
-        // both prime numbers
-        let l_hand = 1_299_709;
-        let r_hand = 56_999;
-
-        let proof = 1;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn coprime_odd_test() {
-        let l_hand = 2_559_031_471i64; // 150531263ᵖ ⋅17ᵖ
-        let r_hand = 1_956_912_061; // 150531697ᵖ ⋅13ᵖ
-
-        let proof = 1;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn coprime_mixed_a_test() {
-        let l_hand = 52_685_751_650_i64; // 150530719ᵖ ⋅350ᶜ
-        let r_hand = 52_535_230_703_i64; // 150530747ᵖ ⋅349ᵖ
-
-        let proof = 1;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn coprime_mixed_b_test() {
-        let l_hand = 19_209_934_347_i64; // 56666473ᵖ ⋅113ᵖ ⋅3ᵖ
-        let r_hand = 10_993_312_058_i64; // 56666557ᵖ ⋅2ᵖ ⋅97ᵖ
-
-        let proof = 1;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn not_coprime_mixed_a_test() {
-        let l_hand = 37_683_426; // 570961ᵖ ⋅66ᶜ
-        let r_hand = 18_804_423; // 569831ᵖ ⋅33ᶜ
-
-        let proof = 33;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn not_coprime_mixed_b_test() {
-        let l_hand = 1_822_623; // 5021ᵖ ⋅33ᶜ ⋅11ᵖ
-        let r_hand = 1_650_990; // 5003ᵖ ⋅10ᶜ ⋅33ᶜ
-
-        let proof = 33;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn not_coprime_extra_test() {
-        let l_hand = 55_286_231; // 5021ᵖ ⋅77ᶜ ⋅11ᵖ ⋅13ᵖ
-        let r_hand = 7_704_620; // 5003ᵖ ⋅10ᶜ ⋅154ᶜ
-
-        let proof = 77;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn not_coprime_divisor_is_gcd_a() {
-        let l_hand = 777_777_777;
-        let r_hand = 111_111_111;
-
-        run_test(l_hand, r_hand, r_hand)
-    }
-
-    #[test]
-    fn not_coprime_divisor_is_gcd_b() {
-        let num = 777_777_777;
-
-        run_test(num, num, num)
-    }
-
-    #[test]
-    fn not_coprime_odd_a_test() {
-        let l_hand = 3_150_055_839i64; // 150002659ᵖ ⋅7ᵖ ⋅3ᵖ
-        let r_hand = 76_604_397; // 1502047ᵖ ⋅17ᵖ ⋅3ᵖ
-
-        let proof = 3;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn not_coprime_odd_b_test() {
-        let l_hand = 56_991; // 157ᵖ ⋅33ᶜ ⋅11ᵖ
-        let r_hand = 49_599; // 167ᵖ ⋅9ᶜ ⋅33ᶜ
-
-        let proof = 33;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn not_coprime_even_a_test() {
-        let l_hand = 549_755_813_888i64; // 2³⁹
-        let r_hand = 300_005_318; // 150002659ᵖ ⋅2ᵖ
-
-        let proof = 2;
-
-        run_test(l_hand, r_hand, proof)
-    }
-
-    #[test]
-    fn not_coprime_even_b_test() {
-        let l_hand = 549_755_813_888i64; // 2³⁹
-        let r_hand = 33_554_432; // 2²⁵            
-
-        run_test(l_hand, r_hand, r_hand)
+            run_test(l_hand, r_hand, r_hand)
+        }
     }
 }
 
